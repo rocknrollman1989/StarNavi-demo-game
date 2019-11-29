a74fd63e1afdd9231a5f0d221920c8a5c2a2eb80
Ilya Volkov <retrocarman1989@gmail.com>
2019-11-29 10:40:12 +0200

    Updates

---
+++ precache-manifest.5b743fa6368622cdb5fad1052811533f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b1ed00849aff6f006b18c138fab5880",
+    "url": "/StarNavi-demo-game/index.html"
+  },
+  {
+    "revision": "6421ff494e326f024444",
+    "url": "/StarNavi-demo-game/static/css/main.51e8283a.chunk.css"
+  },
+  {
+    "revision": "6d836388309ba7784a94",
+    "url": "/StarNavi-demo-game/static/js/2.7a78eb58.chunk.js"
+  },
+  {
+    "revision": "6421ff494e326f024444",
+    "url": "/StarNavi-demo-game/static/js/main.61fce090.chunk.js"
+  },
+  {
+    "revision": "5d471dc702796d905bac",
+    "url": "/StarNavi-demo-game/static/js/runtime~main.66b2d419.js"
+  }
+]);
